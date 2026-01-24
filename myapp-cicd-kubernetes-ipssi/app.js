504830ae9e47617a20d3f07f3be4b2e4b25b3052
Navid Sabete <nevid.dev@gmail.com>
2026-01-24 12:27:18 +0000

    create a minimal app - v0 to prepare before deployment & further versions

---
+++ myapp-cicd-kubernetes-ipssi/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const VERSION = "v0";
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>My App - IPSSI</h1>
+    <p>Version: ${VERSION}</p>
+    <p>Hello from Argo CD 🚀</p>
+  `);
+});
+
+const PORT = 8080;
+app.listen(PORT);
